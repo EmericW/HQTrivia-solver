6a27ce24b8b0dbdbe9efd18fe072641d95713a68
emeric <emeric.warnez@student.howest.be>
2018-10-26 21:32:25 +0200

    Add sharp for image cropping

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+// temporary forever fix
+setInterval(function() {
+	console.log('timer that keeps nodejs processing running');
+}, 1000 * 60 * 60);
+
 const fs = require('fs');
+const sharp = require('sharp');
 
 const newFileFound = filename => {
 	console.log(filename);
+
+	sharp('test.png')
+		.extract({ left: 0, top: 0, width: 500, height: 500 })
+		.toFile('test-crop.png', function(err) {
+			console.log(err);
+			// output.jpg is a 300 pixels wide and 200 pixels high image
+			// containing a scaled and cropped version of input.jpg
+		});
 };
 
 const getDirectoryFiles = path => {
@@ -18,5 +32,6 @@ const watchForNewFile = path => {
 	});
 };
 
-let files = getDirectoryFiles(__dirname);
-setInterval(() => watchForNewFile(__dirname), 1000);
+// let files = getDirectoryFiles(__dirname);
+// setInterval(() => watchForNewFile(__dirname), 1000);
+newFileFound('test.png');
