027829bd8dac5e0fb0be107f983fcb97714443fe
emeric <emeric.warnez@student.howest.be>
2018-10-27 18:10:05 +0200

    Basic answer solving

--- index.js
+++ index.js
@@ -6,23 +6,44 @@ setInterval(function() {
 const fs = require('fs');
 const sharp = require('sharp');
 const tesseract = require('tesseract.js');
+const wiki = require('wikijs');
+const https = require('https');
+const SUBSCRIPTION_KEY = 'asdf';
+const googleKey = 'AIzaSyCcqQ6yx0aVMrsHG7Zo86f_hJoOreAVMWg';
 
 const newFileFound = filename => {
-	sharp('test.png')
+	sharp(filename)
 		.extract({ left: 40, top: 250, width: 670, height: 650 })
-		.toFile('test-crop.png', function(err) {
-			tesseract
-				.recognize(`${__dirname}/test-crop.png`, {
-					lang: 'eng',
-				})
-				.then(result => {
-					const part = result.text.split('?');
-					const q = part[0].split('\n').join(' ');
-					const as = part[1].split('\n').filter(i => i);
-					console.log(`Question: ${q}`);
-					console.log('Answers: ');
-					as.forEach((a, i) => console.log(`\t${i}: ${a}`));
-				});
+		.toFile('testImages/tmp-crop.png', async err => {
+			const result = await tesseract.recognize(`${__dirname}/testImages/tmp-crop.png`, {
+				lang: 'eng',
+			});
+			// fs.unlinkSync('testImages/tmp-crop.png');
+			const part = result.text.split('?');
+			const q = part[0].split('\n').join(' ');
+			const as = part[1].split('\n').filter(i => i);
+			console.log(`Question: ${q}`);
+			console.log('Answers: ');
+			as.forEach((a, i) => console.log(`\t${i}: ${a}`));
+			// const asdf = await wiki().search(q, 5);
+			// console.log(asdf);
+			let content = '';
+
+			https.get(
+				{
+					hostname: 'www.google.com',
+					path: '/search?q=' + encodeURIComponent(q),
+				},
+				res => {
+					res.on('data', part => (content += part));
+					res.on('end', () => {
+						console.log(content.toLowerCase().match(/\bbusking\b/gm));
+					});
+					res.on('error', e => {
+						throw e;
+					});
+				}
+			);
 		});
 };
 
@@ -42,4 +63,4 @@ const watchForNewFile = path => {
 
 // let files = getDirectoryFiles(__dirname);
 // setInterval(() => watchForNewFile(__dirname), 1000);
-newFileFound('test.png');
+newFileFound('testImages/test.png');
