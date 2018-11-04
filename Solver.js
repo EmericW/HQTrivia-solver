ad3c982ff03af7b1a74fff9591a8438cf3a73e42
emeric <emeric.warnez@student.howest.be>
2018-11-04 17:10:05 +0100

    Added search by question + event emitter

--- Solver.js
+++ Solver.js
@@ -1,14 +1,15 @@
 const https = require('https');
+const events = require('events');
 const request = require('request');
 const { parse } = require('node-html-parser');
 
 const calculatePercentages = answers => {
 	let total = 0;
 	Object.keys(answers).forEach(key => {
-		total += answers[key];
+		total += answers[key] || 0;
 	});
 	Object.keys(answers).forEach(key => {
-		answers[key] = (answers[key] / total) * 100;
+		answers[key] = (answers[key] / total || 0) * 100;
 	});
 	return answers;
 };
@@ -28,33 +29,78 @@ const getTopSearchResults = (question, top, callback) => {
 		const dom = parse(content);
 		const searchResults = dom
 			.querySelectorAll('.r a')
-			.map(e => e.attributes.href.substr(7))
-			.slice(0, top);
+			.map(e => e.attributes.href.substr(7).split('&')[0])
+			.slice(1, top + 1);
 		callback(null, searchResults);
 	});
 };
 
-const solve = (question, answers, callback) => {
-	const result = {};
-	answers.forEach(a => (result[a] = 0));
+const scoreContent = (content, target, callback) => {
+	const regex = new RegExp(`${target.toLowerCase()}`, 'g');
+	const matches = content.toLowerCase().match(regex);
+	return (matches && matches.length) || 0;
+};
+
+const searchByAnswers = (question, answers, callback) => {
 	let count = 0;
-	getTopSearchResults(question, 5, (err, results) => {
-		results.forEach(link => {
-			resolveUrl(link.split('&')[0], (err, content) => {
-				answers.forEach(answer => {
-					let score = 0;
-					const expresion = `${answer.toLowerCase()}`;
-					const regex = new RegExp(expresion, 'g');
-					const matches = content.toLowerCase().match(regex);
-					result[answer] += (matches && matches.length) || 0;
+	let scores = {};
+
+	answers.forEach(answer => {
+		getTopSearchResults(answer, 5, (err, results) => {
+			results.forEach(link => {
+				resolveUrl(link, (err, content = '') => {
+					const score = scoreContent(content, question);
+					if (!scores[answer]) {
+						scores[answer] = score;
+					}
+					scores[answer] += score;
+
 					count++;
-					if (count >= 15) {
-						callback(calculatePercentages(result));
+					if (count === 16) {
+						callback(scores);
 					}
 				});
 			});
 		});
 	});
+};
+
+const searchByQuestion = (question, answers, callback) => {
+	let count = 0;
+	let scores = {};
+
+	getTopSearchResults(question, 5, (err, results) => {
+		results.forEach(link => {
+			resolveUrl(link, (err, content = '') => {
+				answers.forEach(answer => {
+					const score = scoreContent(content, answer);
+
+					if (!scores[answer]) {
+						scores[answer] = score;
+					}
+					scores[answer] += score;
+
+					count++;
+					if (count >= 15) {
+						callback(scores);
+					}
+				});
+			});
+		});
+	});
+};
+
+const solve = (question, answers, callback) => {
+	const emitter = new events.EventEmitter();
+	const probabilities = {};
+	answers.forEach(a => (probabilities[a] = 0));
+	searchByQuestion(question, answers, result => {
+		emitter.emit('data', calculatePercentages(result));
+	});
+	searchByAnswers(question, answers, result => {
+		emitter.emit('data', calculatePercentages(result));
+	});
+	callback(emitter);
 };
 
 module.exports = solve;
--- test.js
+++ test.js
@@ -1,8 +1,14 @@
 const data = require('./data.json');
 const solve = require('./Solver');
 
-data.forEach(question => {
-	solve(question.question, question.answers, answer => {
-		console.log(answer);
+// data.forEach(question => {
+// 	solve(question.question, question.answers, answer => {
+// 		console.log(answer);
+// 	});
+// });
+
+solve(data[0].question, data[0].answers, emitter => {
+	emitter.on('data', data => {
+		console.log(data);
 	});
 });
